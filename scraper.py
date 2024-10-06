159ca2b78a6a4f78943424e037e7c6ba54383fc9
rj8 <rj8adhikari@gmail.com>
2024-10-06 15:44:17 -0500

    Scrapes some headlines, still more work to make it scrape all the headlines

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import bs4
+import requests
+
+windex = 0
+
+info = requests.get('https://finance.yahoo.com/topic/stock-market-news')
+# print(info.text)
+
+soup = bs4.BeautifulSoup(info.text, 'html.parser')
+print(soup.title)
+
+headlines = soup.find_all('p')
+# print(headlines[3])
+
+for i in headlines:
+    if "Most Read from Bloomberg" in str(i):
+        splitted = str(i).split("Most Read from Bloomberg")
+        if "--" in splitted[0]:
+            splittedier = splitted[0].split("--")
+            print(splittedier[-1])
+        # print(splitted[0])
+        else:
+            continue
+    else:
+        continue
+
+prg = soup.select('p')
+# print(prg)
